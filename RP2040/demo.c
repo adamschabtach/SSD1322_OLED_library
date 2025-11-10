f242a109df812cb63bb17f1da8404533fd64726e
Adam Schabtach <adam@audiodamage.com>
2025-11-10 11:57:30 -0700

    Tidied demo.c

--- RP2040/demo.c
+++ RP2040/demo.c
@@ -10,21 +10,30 @@
 //#include "../SSD1322_OLED_lib/Fonts/FreeMono12pt7b.h"
 //#include "../SSD1322_OLED_lib/Fonts/FreeSansOblique9pt7b.h"
 
-
-
 int main()
 {
+	absolute_time_t currentTime, nextTime;
+	// Initialize time tracking
+	currentTime = get_absolute_time();
+	nextTime = delayed_by_ms(currentTime, 1000);
   	// Declare bytes array for a frame buffer.
 	// Dimensions are divided by 2 because one byte contains two 4-bit grayscale pixels
 	uint8_t tx_buf[256 * 64 / 2];
 	bool ledState = false;	
-	int px[256], py[256], pl[256];
+	int px[256], py[256], pl[256], dl[256], dx[256], dy[256];
 	int pxCount = 22;
 	
 	for (int i = 0; i < pxCount; i++) {
 		px[i] = get_rand_32() % 256;
 		py[i] = get_rand_32() % 64;
 		pl[i] = get_rand_32() % 16;
+		dl[i] = get_rand_32() % 2 == 1 ? 1 : -1;;
+		dx[i] = get_rand_32() % 3 - 1;
+		if (dx[i] == 0) {
+			dy[i] = get_rand_32() % 2 == 1 ? 1 : -1;
+		} else {
+			dy[i] = get_rand_32() % 3 - 1;
+		}
 	}
 
     stdio_init_all();
@@ -43,19 +52,31 @@ int main()
 		fill_buffer(tx_buf, 0);
 		for (int i = 0; i < pxCount; i++) {
 			draw_pixel(tx_buf, px[i], py[i], pl[i]);
-			pl[i]--;
-			px[i]++;
-			if (pl[i] < 0 || px[i] >= 256) {
+			pl[i] += dl[i];
+			if (pl[i] == 0 || pl[i] == 15) {
+				dl[i] = -dl[i];
+			}
+			px[i] += dx[i];
+			py[i] += dy[i];
+			if (px[i] < 0 || px[i] >= 256 || py[i] < 0 || py[i] >= 64) {
 				px[i] = get_rand_32() % 256;
 				py[i] = get_rand_32() % 64;
 				pl[i] = get_rand_32() % 16;
+				dl[i] = get_rand_32() % 2 == 1 ? 1 : -1;;
+				dx[i] = get_rand_32() % 3 - 1;
+				if (dx[i] == 0) {
+					dy[i] = get_rand_32() % 2 == 1 ? 1 : -1;
+				} else {
+					dy[i] = get_rand_32() % 3 - 1;
+				}
 			}
 		}
 		// send a frame buffer to the display
 		send_buffer_to_OLED(tx_buf, 0, 0);
-  //      gpio_put(PICO_DEFAULT_LED_PIN, ledState);
-		ledState = !ledState;
-//		sleep_ms(100 + (get_rand_32() % 2000));
-	//	printf("tick\n");
+		if (absolute_time_diff_us(get_absolute_time(), nextTime) < 0) {
+			nextTime = delayed_by_ms(nextTime, 1000);
+  	      	gpio_put(PICO_DEFAULT_LED_PIN, ledState);
+			ledState = !ledState;
+		}
     }
 }
